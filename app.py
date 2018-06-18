b6a67071633e0be90e83f16ebc7deac5cb3e7b27
shughes1109 <40275719+shughes1109@users.noreply.github.com>
2018-06-17 22:18:14 -0400

    Updating app with textAnalytics(kinda)

--- app.py
+++ app.py
@@ -10,6 +10,9 @@ load_dotenv(dotenv_path)
 
 KEY2    = environ.get('KEY2')
 URL_API = environ.get('URL_API')
+KEY4    = environ.get('KEY4')
+URL_API2 = environ.get('URL_API2')
+
 
 app = Flask(__name__)
 
@@ -44,6 +47,39 @@ def visual():
             message = 'send the parameters'
            )
 
+    
+  @app.route('/text', methods = ['POST', 'GET'])
+def text():
+    if request.method == 'POST':
+        url = URL_API2
+
+        querystring = {"maxCandidates":"1"} # para los params, recuerdo que esto te dejaba la mejor prediccion. dado que no puedo testear este codigo, no se si se mantendra lo mismo, o sera otra forma de presentar.
+                #params = urllib.parse.urlencode({  }) esto es lo que sale en los ejemplos como params, pero nuevamente, sin testeo, lo deje como estaba
+       documents = {'documents' : [
+ 	 {'id': '1', 'language': 'en', 'text': 'I had a wonderful experience! The rooms were wonderful and the staff was helpful.'},
+  	 {'id': '2', 'language': 'es', 'text': 'Los caminos que llevan hasta Monte Rainier son espectaculares y hermosos.'}
+	]}  
+
+
+ headers = {
+            'Content-Type': "application/json",
+            'Ocp-Apim-Subscription-Key': KEY4,
+            'Cache-Control': "no-cache",
+            'Postman-Token': "a77b2af8-05c4-160b-a138-5b8d610428fa" #En teoria, esto deberia estar bueno, si es que usa el mismo token desde Postman, pero no cacho.
+            }
+
+        response = requests.request("POST", url, data=documents, headers=headers, params=querystring)
+
+        print(response.text)
+        return jsonify(
+            status = response.text
+           )
+    else:
+        return jsonify(
+            message = 'send the parameters'
+           )
+ 
+    
 @app.route('/send_data_db', methods = ['POST', 'GET'])
 def send_data_db():
     pass
